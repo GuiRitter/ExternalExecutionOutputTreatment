57b4b65720ab68920daa5769c1059b4489b6c4f0
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2017-12-20 22:32:27 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# External Execution Output Treatment
+
+Executes Linux command line commands and the code to treat each line of output, be it regular output or error output, can (must) be implemented at will for each line. Commands are passed as an array of arguments, where the first element in the array is the command and the following ones are its arguments.
---
+++ src/io/github/guiritter/external_execution_output_treatment/ExternalExecutionOutputTreatment.java
@@ -0,0 +1,62 @@
+package io.github.guiritter.external_execution_output_treatment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@SuppressWarnings("CallToPrintStackTrace")
+public abstract class ExternalExecutionOutputTreatment {
+
+    final class ErrorReaderRunnable extends ReaderRunnable {
+
+        @Override
+        void treatLine(String line) {
+            treatErrorLine(line);
+        }
+
+        public ErrorReaderRunnable(BufferedReader reader) {
+            super(reader);
+        }
+    }
+
+    final class InputReaderRunnable extends ReaderRunnable {
+
+        @Override
+        void treatLine(String line) {
+            treatInputLine(line);
+        }
+
+        public InputReaderRunnable(BufferedReader reader) {
+            super(reader);
+        }
+    }
+
+    private Process process;
+
+    public final void execute(String commands[]) throws IOException, InterruptedException {
+        process = Runtime.getRuntime().exec(commands);
+        new Thread(new ErrorReaderRunnable(new BufferedReader(
+         new InputStreamReader(process.getErrorStream())))).start();
+        new Thread(new InputReaderRunnable(new BufferedReader(
+         new InputStreamReader(process.getInputStream())))).start();
+        process.waitFor();
+    }
+
+    public abstract void treatErrorLine(String line);
+
+    public abstract void treatInputLine(String line);
+
+//    public static void main(String[] args) throws IOException, InterruptedException {
+//        new ExternalExecutionOutputTreatment() {
+//            @Override
+//            public void treatErrorLine(String line) {
+//                System.err.println(line);
+//            }
+//
+//            @Override
+//            public void treatInputLine(String line) {
+//                System.out.println(line);
+//            }
+//        }.execute(new String[]{"wmctrl", "-a", "Terminal"});
+//    }
+}
